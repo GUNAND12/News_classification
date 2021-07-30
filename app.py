6cc14044049da184171624afe1018e8219e9166f
Gunand Mayanglmbm <40438996+GUNAND12@users.noreply.github.com>
2021-07-30 15:10:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
 
 if __name__ == "__main__":
 
-    #app.run(debug=True)
-    app.run(host='0.0.0.0',port=8080)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0',port=8080)
 
     logging.info("App is Running Perfectly")
